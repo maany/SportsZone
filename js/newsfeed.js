4c3bd1b9eba5b4e1f719e26e5421fb947f348f0b
maany <imptodefeat@gmail.com>
2016-04-22 13:09:45 +0530

    final

--- js/newsfeed.js
+++ js/newsfeed.js
@@ -6,12 +6,18 @@ $(document).ready(function () {
     try {
         var FEED_URL = "http://www.espncricinfo.com/rss/content/story/feeds/968923.xml";
         var feed_element = document.getElementById("sidebar")
+        // LOOP ITERATIONS
+        var i = 0;
         $.ajax({
             url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(FEED_URL),
             dataType: 'json',
             success: function (data) {
                 if (data.responseData.feed && data.responseData.feed.entries) {
                     $.each(data.responseData.feed.entries, function (i, e) {
+                        i++;
+                        if(i>6) {
+                            return false;
+                        }
                         console.log("------------------------");
                         console.log("title      : " + e.title);
                         console.log("author     : " + e.author);
